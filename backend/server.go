724779222099b661e4ee2078a760347a0360288a
iwdsoon <lucio@shibui.ar>
2024-11-25 15:14:50 -0300

    tables

--- backend/server.go
+++ backend/server.go
@@ -31,4 +31,67 @@ func main() {
 
 	// Start server
 	e.Logger.Fatal(e.Start(config.GetString("httpPort")))
+
+	'CREATE DATABASE `puchito`;
+
+'CREATE TABLE `generos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `genero` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `genero_UNIQUE` (`genero`)
+)
+
+'CREATE TABLE `libros` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `autor` varchar(45) NOT NULL,
+  `fecha_lanzamiento` datetime NOT NULL,
+  `id_genero` int NOT NULL,
+  `fecha_creado` datetime DEFAULT CURRENT_TIMESTAMP,
+  `fecha_actualizado` datetime DEFAULT CURRENT_TIMESTAMP,
+  `estado` tinyint(1) DEFAULT ''1'',
+  PRIMARY KEY (`id`),
+  KEY `fk_genero_idx` (`id_genero`),
+  CONSTRAINT `fk_genero` FOREIGN KEY (`id_genero`) REFERENCES `generos` (`id`)
+)
+
+'CREATE TABLE `roles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `rol` varchar(10) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `rol_UNIQUE` (`rol`)
+)
+
+'CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(45) NOT NULL,
+  `apellido` varchar(45) NOT NULL,
+  `dni` varchar(8) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `fecha_creado` datetime DEFAULT CURRENT_TIMESTAMP,
+  `fecha_actualizado` datetime DEFAULT CURRENT_TIMESTAMP,
+  `estado` tinyint(1) DEFAULT ''1'',
+  `id_rol` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `dni_UNIQUE` (`dni`),
+  UNIQUE KEY `email_UNIQUE` (`email`),
+  KEY `fk_rol_idx` (`id_rol`),
+  CONSTRAINT `fk_rol` FOREIGN KEY (`id_rol`) REFERENCES `roles` (`id`)
+)
+
+
+'CREATE TABLE `prestamos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_usuario` int NOT NULL,
+  `id_libro` int NOT NULL,
+  `fecha_prestamo` datetime DEFAULT CURRENT_TIMESTAMP,
+  `fecha_devolucion_estimada` datetime DEFAULT NULL,
+  `fecha_devolucion` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_usuario_idx` (`id_usuario`),
+  KEY `fk_libro_idx` (`id_libro`),
+  CONSTRAINT `fk_libro` FOREIGN KEY (`id_libro`) REFERENCES `libros` (`id`),
+  CONSTRAINT `fk_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`)
+)
 }
